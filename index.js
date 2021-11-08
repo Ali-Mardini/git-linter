5a505175211b3bf57eb42a0dd18fd9a36d05f264
Ali Mardini <mardinia6@gmail.com>
2021-11-08 12:54:48 +0400

    feat(new): give the user option to choose which package manger he is using

--- index.js
+++ index.js
@@ -1,79 +1,112 @@
 const { exec } = require('child_process');
 const fs = require('fs');
-const packageFile = require(__dirname + '/package.json');
+const packageFile = require('../../package.json');
+const inquirer = require('inquirer');
 
 
-// lint script
-var lintScript = "eslint --fix";
+// create a question list
+inquirer
+    .prompt([
+        {
+            type: 'list',
+            name: 'packageManager',
+            message: 'Which package manage are you using?',
+            choices: [
+                'npm',
+                'yarn'
+            ],
+        }
+    ])
+    .then((answers) => {
+        console.log(JSON.stringify(answers, null, '  '));
+        if (answers.packageManager == 'npm') {
+            // create hooks with npm configurations
+            createHooks('npm');
+        } else {
 
-// check if lint script exist
-if (packageFile.scripts.lint != '' || packageFile.scripts.lint == undefined) {
-    // add to package.json
-    packageFile.scripts.lint = lintScript;
+            // create hooks with yarn configurations
+            createHooks('yarn');
+        }
+    });
 
-    // write to the file
-    fs.writeFileSync(__dirname + '/package.json', JSON.stringify(packageFile));
-    console.log("JSON data is saved.");
-}
 
-// check if the test scripts exist in package.json
-if (packageFile.scripts.test == '' || packageFile.scripts.test == undefined) {
-    console.log('No Test Script is specified!');
-}
+const createHooks = (option) => {
+    //lint script
+    var lintScript = "eslint --fix";
 
-// bash script to run it as commit msg
-var commitMsg =
-    '#!/bin/bash \n' +
+    // check if lint script exist
+    if (packageFile.scripts.lint == '' || packageFile.scripts.lint == undefined) {
+        // add to package.json
+        packageFile.scripts.lint = lintScript;
 
-    '# Color variables \n' +
-    "red='\033[0;31m' \n" +
-    '# Clear the color after that \n' +
-    "clear='\033[0m' \n" +
-
-    'if ! head -1 "$1" | grep -qE "^(feat|fix|chore|docs|test|style|refactor|perf|build|ci|revert)(\(.+?\))?: .{1,}$"; then \n' +
-
-    '    echo -e "${red}Aborting commit. Your commit message is invalid. Please follow this format e.g: feat(scope): Initial commit${clear}" >&2 \n' +
-    '    exit 1 \n' +
-    'fi \n' +
-    'if ! head -1 "$1" | grep -qE "^.{1,88}$"; then \n' +
-    '    echo "Aborting commit. Your commit message is too long." >&2 \n' +
-    '    exit 1 \n' +
-    'fi \n';
-
-// create commit msg file in hooks folder 
-fs.writeFileSync('../../.git/hooks/commit-msg', commitMsg);
-
-// make hook file executable
-exec("chmod +x ../../.git/hooks/commit-msg", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+        // write to the file
+        fs.writeFileSync('../../package.json', JSON.stringify(packageFile));
+        console.log("JSON data is saved.");
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-});
 
-// bash script run as pre commit hook
-var preCommit = `
-#!/bin/sh
-npm run lint && npm test
-`;
-
-// create commit msg file in hooks folder 
-fs.writeFileSync('../../.git/hooks/pre-commit', preCommit);
-
-// make hook file executable
-exec("chmod +x ../../.git/hooks/pre-commit", (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
+    // check if the test scripts exist in package.json
+    if (packageFile.scripts.test == '' || packageFile.scripts.test == undefined) {
+        console.log('No Test Script is specified!');
     }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
+
+    // bash script to run it as commit msg
+    var commitMsg =
+        '#!/bin/bash \n' +
+
+        '# Color variables \n' +
+        "red='\033[0;31m' \n" +
+        '# Clear the color after that \n' +
+        "clear='\033[0m' \n" +
+
+        'if ! head -1 "$1" | grep -qE "^(feat|fix|chore|docs|test|style|refactor|perf|build|ci|revert)(\(.+?\))?: .{1,}$"; then \n' +
+
+        '    echo -e "${red}Aborting commit. Your commit message is invalid. Please follow this format e.g: feat(scope): Initial commit${clear}" >&2 \n' +
+        '    exit 1 \n' +
+        'fi \n' +
+        'if ! head -1 "$1" | grep -qE "^.{1,88}$"; then \n' +
+        '    echo "Aborting commit. Your commit message is too long." >&2 \n' +
+        '    exit 1 \n' +
+        'fi \n';
+
+    // create commit msg file in hooks folder 
+    fs.writeFileSync('../../.git/hooks/commit-msg', commitMsg);
+
+    // make hook file executable
+    exec("chmod +x ../../.git/hooks/commit-msg", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+    });
+
+    // bash script run as pre commit hook
+    var preCommit = '';
+
+    // check if the package manager is npm or yarn
+    if (option == 'npm') {
+        preCommit = '#!/bin/sh \n' +
+            'npm run lint && npm test';
+    } else {
+        preCommit = '#!/bin/sh \n' +
+            'yarn lint && yarn test';
     }
-    console.log(`stdout: ${stdout}`);
-});
+
+    // create commit msg file in hooks folder 
+    fs.writeFileSync('../../.git/hooks/pre-commit', preCommit);
+
+    // make hook file executable
+    exec("chmod +x ../../.git/hooks/pre-commit", (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message} `);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr} `);
+            return;
+        }
+    });
+};
